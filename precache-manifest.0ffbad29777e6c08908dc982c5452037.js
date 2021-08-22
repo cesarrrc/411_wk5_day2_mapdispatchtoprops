cc88d733a93aa3887149dce143f2a053e64981a0
Cesar Cisneros <cesarcisneros9@gmail.com>
2021-08-21 23:22:02 -0500

    Updates

---
+++ precache-manifest.0ffbad29777e6c08908dc982c5452037.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1282fbb01bc6ba857e4f0d47497553",
+    "url": "/411_wk5_day2_mapdispatchtoprops/index.html"
+  },
+  {
+    "revision": "6f930ae790f4d1580df9",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "9c05d681c587be15a369",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/2.d668e44e.chunk.js"
+  },
+  {
+    "revision": "6f930ae790f4d1580df9",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/main.21dae2d4.chunk.js"
+  },
+  {
+    "revision": "e47b0be5e9874947a62d",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/runtime~main.df0822c9.js"
+  }
+]);
